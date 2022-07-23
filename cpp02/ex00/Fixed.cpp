ed5c6da46004b514e3bfc470a5b54c563edc7375
imabid99 <imabid@student.1337.ma>
2022-07-23 11:48:14 +0100

    CPP

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 14:21:28 by imabid            #+#    #+#             */
-/*   Updated: 2022/07/23 11:38:17 by imabid           ###   ########.fr       */
+/*   Updated: 2022/07/23 11:47:55 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ Fixed::Fixed(const Fixed &other)
     std::cout << "Copy constructor called" << std::endl;
     this->fix_p = other.getRawBits();
 }
-Fixed::Fixed
+// Fixed::Fixed
 int Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
