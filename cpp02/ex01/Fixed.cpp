167fe589135b63ada07d41ededb131964b0913a6
Imad Abid <imabid@student.1337.ma>
2022-09-01 16:02:39 +0100

    CPP

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:02:40 by imabid            #+#    #+#             */
-/*   Updated: 2022/09/01 15:48:00 by imabid           ###   ########.fr       */
+/*   Updated: 2022/09/01 16:00:45 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,19 @@ Fixed::Fixed()
 Fixed::Fixed(int raw)
 {
     std::cout << "Int constructor called" << std::endl;
-    fix_p = raw << frac_bits;
-    std::cout << "this is fix_p in int = " << fix_p << std::endl;
-    
+    fix_p = raw << frac_bits; 
 }
 
 Fixed::Fixed(float raw)
 {
     std::cout << "Float constructor called" << std::endl;
     fix_p = (int)roundf(raw * (1 << frac_bits));
-    std::cout << "this is fix_p int float = " << fix_p << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called" << std::endl;
-    this->fix_p = other.getRawBits();
-     std::cout << "this is fix_p in int in copy = " << fix_p << std::endl;
+    *this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
@@ -64,13 +60,12 @@ Fixed::~Fixed()
 
 int Fixed::toInt(void) const
 {
-    return((int)this->fix_p >> frac_bits);
+    return (this->fix_p) / (1 << frac_bits);
 }
 
 float Fixed::toFloat(void) const
 {
-     std::cout << "this is fix_p int to float = " << (float(this->fix_p) / (1 << frac_bits)) << std::endl;
-    return (float(this->fix_p) / (1 << frac_bits));
+    return float(this->fix_p) / (1 << frac_bits);
 }
 
 std::ostream&   operator<<(std::ostream &out, const Fixed &fixed)
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 17:03:15 by imabid            #+#    #+#             */
-/*   Updated: 2022/09/01 15:34:01 by imabid           ###   ########.fr       */
+/*   Updated: 2022/09/01 15:51:37 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ int main( void ) {
     Fixed const b( 10 );
     Fixed const c( 42.42f );
     Fixed const d( b );
+
     a = Fixed( 1234.4321f );
+    
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
     std::cout << "d is " << d << std::endl;
+    
     std::cout << "a is " << a.toInt() << " as integer" << std::endl;
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
