76845a8d19f0b8000e97f3a50791e815ff918689
Imad Abid <imabid@student.1337.ma>
2022-09-12 13:22:04 +0100

    CPP04

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 10:37:47 by imabid            #+#    #+#             */
+/*   Updated: 2022/09/12 10:49:33 by imabid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat{
+    private:
+        std::string const name;
+        int grade;
+    public:
+        std::string getName();
+        int getGrade();
+};
+
+#endif
