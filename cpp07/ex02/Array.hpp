844e8cb448d25d6d3f5b2e5e2a3999bf350882a9
Imad Abid <imabid@student.1337.ma>
2022-09-22 13:21:16 +0100

    CPP

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,55 @@
 /*   By: imabid <imabid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 15:20:32 by imabid            #+#    #+#             */
-/*   Updated: 2022/09/21 15:20:33 by imabid           ###   ########.fr       */
+/*   Updated: 2022/09/22 13:18:47 by imabid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Array
+{
+    private:
+        T *arr;
+        unsigned int length;
+    public:
+    Array() : arr(0), n(0);
+    Array(unsigned int n)
+    {
+        this->length = n;
+        arr = new T[n];
+    };
+    Array &operator=(Array const &other)
+    {
+	    if (this != &other)
+	    {
+	    	this->_len = other._len;
+	    	this->_arr = new T[other.length];
+	    	for (int i = 0; i < length; i++)
+	    		this->_arr[i] = other._arr[i];
+	    }
+	    return (*this);
+	};
+    T  &operator[](unsigned int ind)
+    {
+        if(ind >= this->length)
+            throw std::out_of_range ("index is out of bounds");
+        else
+            return this->arr[ind];
+    }
+    int const size() const
+    {
+        return this->length;
+    }
+    ~Array()
+    {
+        delete [] arr;
+    }
+};
+
+#endif
